cd349f315b3bd456efae68542a8bcf7f3298bb95
George Czabania <dev@stayradiated.com>
2012-05-17 10:35:02 +1200

    [bugfix] List count not updating after a list is deleted.

--- js/ui.js
+++ js/ui.js
@@ -1441,6 +1441,8 @@ var ui = {
 				// The list has one below it -> select it
 				$listToDelete.next().click().prev().remove();
 			}
+			
+			ui.lists.updateCount();
 		},
 
 
@@ -1449,7 +1451,7 @@ var ui = {
 			UPDATE LIST COUNT
 		**********************************/
 
-		updateCount: function (type) {
+		updateCount: function () {
 			$('#sidebar li').map(function () {
 				$(this).find('.count').html(cli.populate('list', $(this).attr('id').substr(1).toNum()).length);
 			});
