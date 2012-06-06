b5a87ffceaf431abe28dd732b38c9e4d8989623a
George Czabania <george@czabania.com>
2012-06-06 20:20:25 +1200

    Initial commit

---
+++ js/plugins/filter.js
@@ -0,0 +1,15 @@
+/* Filters Plugin for Nitro
+ * By Jono Cooper & George Czabania
+ * Licensed under the BSD License
+ */
+
+//Adds as a plugin
+plugin.add(function() {
+	
+	filter = function(list, filters) {
+		
+		
+		
+	}
+	
+});
