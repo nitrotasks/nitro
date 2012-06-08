3338cfd974976ed4873230764c9465f23a9c93ce
George Czabania <george@czabania.com>
2012-06-08 15:27:13 +1200

    Finished filter.js

--- js/plugins/filter.js
+++ js/plugins/filter.js
@@ -10,34 +10,101 @@ plugin.add(function() {
 	
 	filter = function(list, filters) {
 		
+		// This will check one task and either return true or false
 		var check = function(task, key, property) {
 			
-			// Because arrays have a typeof object
+			// Handles multiple properties in an array
 			if(typeof property === 'object') {
-				
 				var match = false;
-				
 				// Loop through this
-				for(var i = 0; i < property.length; i++) {
-										
+				for(var i = 0; i < property.length; i++) {			
 					if(check(task, key, property[i])) {
-						
 						match = true;
-						
 					}
-					
 				}
-				
 				return match;
+			}
+			
+			// Formats the property value
+			switch(key) {
 				
-			} else if(task[key] == property) {
-							
+				case "logged":
+						
+					// Get tasks that are logged
+					if (property === true) {
+						if(typeof task[key] == 'number') return true;
+						
+					// Get tasks that were logged after a certain time
+					} else if (typeof property == 'number') {
+						if(task[key] >= property) return true;
+					}
+					break;
+					
+				case "notes":
+				
+					// Get tasks with notes
+					if (property === true) {
+						// Notes must have at least one non-space char
+						if(task[key].match(/\S/)) return true;
+					}
+					break;
+					
+				case "priority":
+				
+					// Gets tasks without a priority
+					if (property === false) {
+						property = "none";
+					
+					// Get tasks that have a priority
+					} else if (property === true) {
+						if(task[key] !== "none") return true;
+					}
+					break;
+					
+				case "date":
+				
+					if (property === true) {
+						if (task[key] != false) return true;
+					}
+				
+					var due = new Date(task[key]),
+						today = new Date();
+						
+					if (property == 'month') {
+						if (due.getMonth() == today.getMonth()) return true;
+					}
+				
+					// Copy date parts of the timestamps, discarding the time parts.
+					var one = new Date(due.getFullYear(), due.getMonth(), due.getDate());
+					var two = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+					
+					// Do the math.
+					var millisecondsPerDay = 1000 * 60 * 60 * 24;
+					var millisBetween = one.getTime() - two.getTime();
+					var days = millisBetween / millisecondsPerDay;
+					
+					// Round down.
+					var diff = Math.floor(days);
+					
+					// Get tasks due today
+					if (property == 'today') {
+						if (diff === 0) return true;
+					} else if(property == 'tomorrow') {
+						if (diff <= 1) return true;
+					} else if (property == 'week') {
+						if (diff <= 7) return true;
+					} else if (property == 'fortnight') {
+						if (diff <= 14) return true;
+					} else if (typeof property == 'number') {
+						if (diff <= property) return true;
+					}
+					break;
+			}
+			
+			if(task[key] == property) {
 				return true;
-				
 			} else {
-				
 				return false;
-				
 			}
 		}
 		
@@ -49,6 +116,10 @@ plugin.add(function() {
 			var task = core.storage.tasks[list[i]];
 			
 			for(var key in filters) {
+				
+				// Convert string to boolean
+				if(filters[key] === 'true') filters[key] = true;
+				if(filters[key] === 'false') filters[key] = false;
 				
 				if(check(task, key, filters[key])) {
 					
