dac4413455c2fc50fd23e08992a389dab66902eb
George Czabania <george@czabania.com>
2012-06-08 22:24:34 +1200

    Added priority sorting

--- js/plugins/sort.js
+++ js/plugins/sort.js
@@ -10,6 +10,17 @@ plugin.add(function() {
 	
 	sort = function(list, method) {
 		
+		// Clone list
+		list = list.splice(0);
+		
+		// Convert task IDs to obects
+		for(var i = 0; i < list.length; i++) {
+			var id = list[i];
+			list[i] = core.storage.tasks[list[i]];
+			list[i].arrayID = id;
+		}
+		
+		// Sorting methods
 		switch(method) {
 			
 			case "magic":
@@ -19,12 +30,28 @@ plugin.add(function() {
 				break;
 				
 			case "priority":
+				
+				list.sort(function(a,b) {
+					var worth = { none: 0, low: 1, medium: 2, high: 3 };
+					if(worth[a.priority] > worth[b.priority]) return true;
+					else if(worth[a.priority] < worth[b.priority]) return false;
+					else return null
+				});
+				
 				break;
 				
 			case "date":
 				break;
 			
 		}
+		
+		// Unconvert task IDs to obects
+		for(var i = 0; i < list.length; i++) {
+			console.log(list[i])
+			list[i] = list[i].arrayID;
+		}
+		
+		return list;
 		
 	};
 	
