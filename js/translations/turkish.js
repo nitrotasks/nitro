2002da547005f184dfe7d769b5a881369b31807e
Jono Cooper <jono@jonocooper.com>
2012-06-22 07:53:45 +1200

    Updated Turkish

---
+++ js/translations/turkish.js
@@ -0,0 +1,104 @@
+ui.language({
+	//List Headers
+	focus: "Odak",
+	lists: "Listeler",
+	today: "Bugün",
+	next: "Sonrakiler",
+	scheduled: "Scheduled",
+	logbook: "Logbook",
+	all: "All Tasks",
+
+	//Default Names
+	nlist: "Yeni Liste",
+	ntask: "Yeni Görev",
+
+	//Buttons
+	addbtn: "Ekle", 
+	deletebtn: "Sil", 
+	sortbtn: "Sort",
+
+	// Sort
+	sortMagic: "Magic",
+	sortDefault: "by Hand",
+	sortPriority: "Priority",
+	sortDate: "Date",
+
+	//Placeholder Text
+	search:"Ara", 
+	notes: "Not Ekle",
+
+	//Priority
+	none: "Hiçbiri",
+	low: "Düşük",
+	medium: "Normal",
+	high: "Önemli",
+
+	//Date Labels
+	dueDate: "Due Date",
+	schedule: "Schedule",
+
+	// Delete Modals
+	deleteMore: "Do you really want to delete these %s tasks?",
+	deleteMoreYes: "Yes, Delete them",
+	deleteMoreNo: "No, Keep them",
+	deleteOne: "Do you really want to delete this task?",
+	deleteOneYes: "Yes, Delete it",
+	deleteOneNo: "No, Keep it",
+	deleteList: "Are you sure you want to delete that list?",
+	warning: "Warning!",
+
+	recurring: "Recurring",
+	reviewNo: "Review in:",
+	recurType: "Created:",
+	daily: "daily",
+	weekly: "weekly",
+	monthly: "monthly",
+	recurNext: "Next: ",
+	recurEnds: "Ends: ",
+	reviewAction: "and place in: ",
+	create: "Create",
+	cancel: "Cancel",
+
+	hideWarnings: "Onaylama Uyarılarını Gösterme",
+	nextNoLists: "Hiç görev gösterme",
+	nextEverything:	"Hepsini göster",
+
+	general: "Genel",
+	nextDescription: "Sonrakiler'de her listeden: ", 
+	deleteWarningsDescription: "Bir görev veya listeyi silerken doğrulama kutucuğunu göstermez.",
+
+	language: "Dil",
+	authortext: "Çeviren",
+
+	about: "About",
+	theme: "Tema",
+	pickTheme: "Tema Seçiniz: ",
+	themeDescription: "Temalar Nitro'nun görüntüsünü değiştirmenizi sağlar. Siz de kendi temanızı CSS kullarak oluşturabilirsiniz!",
+	replaceDefault:"Arka planı değiştir: ",
+	useDefault:"Varsayılan arka planı kullanın: ",
+	removeBG: "Arka planı kaldır",
+	bgSize:"Arka plan boyutu: ",
+	fill:"Doldur",
+	shrink:	"Sığdır",
+	tile:"Döşe",
+	bgDescription:"Seçtiğiniz arka planıyla uyumlu olması için yazı rengini değiştiriniz.",
+	headingColor:"Başlık Rengi",
+	default: "Varsayılan",
+	light: "Açık",
+	dark:"Koyu",
+	headingDescription:	"Seçtiğiniz arka planıyla uyumlu olması için yazı rengini değiştiriniz.",
+	sync: "Sync",
+	donateText:	"Sync is beta. If Nitro breaks, explodes or melts your computer - we warned you. It is pretty stable but if anything does break, you can contact <a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> or <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "Run sync: ",
+	syncDescription:"Sync allows you to access your tasks from other computers and smartphones.",
+	syncNever: "Never (No notifications)",
+	syncManual:	"Manually",
+	syncTimer: "Every 2 minutes",
+	syncAuto: "Every time a change is made",
+	syncLoggedIn: "Logged in as: ",
+	syncService:"Service: ",
+	syncLogout: "Logout",
+	syncAuthenticate: "Waiting for Nitro to be authenticated...",
+	syncError: "Could not sync with server...",
+	cancel: "Cancel"
+})
