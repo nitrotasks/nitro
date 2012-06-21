e1661a3e1bcb4c2edf5586273e3bac46d4c617ae
George Czabania <george@czabania.com>
2012-06-21 20:22:10 +1200

    German.js and fixed table layout for languages

---
+++ js/translations/german.js
@@ -0,0 +1,104 @@
+ui.language({
+	//List Headers
+	focus: "Fokus",
+	lists: "Listen",
+	today: "Heute",
+	next: "Demnächst",
+	scheduled: "Scheduled",
+	logbook: "Logbuch",
+	all: "All Tasks",
+
+	//Default Names
+	nlist: "Neue Liste",
+	ntask: "Neue Aufgabe",
+
+	//Buttons
+	addbtn: "Hinzufügen",
+	deletebtn: "Löschen",
+	sortbtn: "Sort",
+
+	// Sort
+	sortMagic: "Magic",
+	sortDefault: "by Hand",
+	sortPriority: "Priority",
+	sortDate: "Date",
+
+	//Placeholder Text
+	search: "Suchen",
+	notes: "Bemerkungen",
+
+	//Priority
+	none: "Keine Priorität",
+	low: "Niedrige Priorität",
+	medium: "Normale Priorität",
+	high: "ohe Priorität",
+
+	//Date Labels
+	dueDate: "Fälligkeitsdatum setzen",
+	schedule: "Schedule",
+
+	// Delete Modals
+	deleteMore: "Do you really want to delete these %s tasks?",
+	deleteMoreYes: "Ja, löschen Sie sie",
+	deleteMoreNo: "Abbrechen",
+	deleteOne: "Soll diese Aufgabe gelöscht werden? Sie wird unwiderruflich gelöscht.",
+	deleteOneYes: "Ja, löschen Sie sie",
+	deleteOneNo: "Abbrechen",
+	deleteList: "Soll diese Liste gelöscht werden? Dadurch werden ebenfalls alle Aufgaben in dieser Liste gelöscht.",
+	warning: "Warnung!",
+
+	recurring: "Recurring",
+	reviewNo: "Review in:",
+	recurType: "Created:",
+	daily: "daily",
+	weekly: "weekly",
+	monthly: "monthly",
+	recurNext: "Next: ",
+	recurEnds: "Ends: ",
+	reviewAction: "and place in: ",
+	create: "Create",
+	cancel: "Cancel",
+
+	hideWarnings: "Keine Lösch-Warnungen anzeigen",
+	nextNoLists: "Don't show any tasks in next",
+	nextEverything:	"Show all tasks",
+
+	general: "General",
+	nextDescription: "Show other lists in Next: ", 
+	deleteWarningsDescription: "This disables the confirm box that appears when you delete a task or list.",
+
+	language: "Language",
+	authortext: "Author",
+
+	about: "About",
+	theme: "Theme",
+	pickTheme: "Pick a theme: ",
+	themeDescription: "Themes allow you to change the look of Nitro. You can even use CSS to create your own!",
+	replaceDefault:"Change the background: ",
+	useDefault: "Use the default background: ",
+	removeBG: "Remove background",
+	bgSize: "Background size: ",
+	fill:"Fill screen",
+	shrink:	"Shrink",
+	tile:"Tile",
+	bgDescription: "Change the color of the text so that it looks nice with your custom background.",
+	headingColor: "Heading color",
+	default: "Default",
+	light: "Light",
+	dark:"Dark",
+	headingDescription:	"Change the color of the text so that it looks nice with your custom background.",
+	sync: "Sync",
+	donateText:	"Sync is beta. If Nitro breaks, explodes or melts your computer - we warned you. It is pretty stable but if anything does break, you can contact <a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> or <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "Run sync: ",
+	syncDescription:"Sync allows you to access your tasks from other computers and smartphones.",
+	syncNever: "Never (No notifications)",
+	syncManual:	"Manually",
+	syncTimer: "Every 2 minutes",
+	syncAuto: "Every time a change is made",
+	syncLoggedIn: "Logged in as: ",
+	syncService:"Service: ",
+	syncLogout: "Logout",
+	syncAuthenticate: "Waiting for Nitro to be authenticated...",
+	syncError: "Could not sync with server...",
+	cancel: "Cancel"
+})
