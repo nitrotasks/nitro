2dbfc288b915d59e1846819a092e6ff67f581642
Jono Cooper <jono@jonocooper.com>
2012-06-22 06:44:29 +1200

    Hungarian

---
+++ js/translations/hungarian.js
@@ -0,0 +1,104 @@
+ui.language({
+	//List Headers
+	focus: "Fókuszban",
+	lists: "Listák",
+	today: "A mai nap",
+	next: "Később",
+	scheduled: "Scheduled",
+	logbook: "Logbook",
+	all: "All Tasks",
+
+	//Default Names
+	nlist: "Új lista",
+	ntask: "Új feladat",
+
+	//Buttons
+	addbtn: "Új",
+	deletebtn: "Delete",
+	sortbtn: "Töröl",
+
+	// Sort
+	sortMagic: "Magic",
+	sortDefault: "by Hand",
+	sortPriority: "Priority",
+	sortDate: "Date",
+
+	//Placeholder Text
+	search: "Keresés",
+	notes: "Megjegyzés",
+
+	//Priority
+	none: "Nincs",
+	low: "Alacsony",
+	medium: "Közöpes",
+	high: "Fontos",
+
+	//Date Labels
+	dueDate: "Due Date",
+	schedule: "Schedule",
+
+	// Delete Modals
+	deleteMore: "Do you really want to delete these %s tasks?",
+	deleteMoreYes: "Yes, Delete them",
+	deleteMoreNo: "No, Keep them",
+	deleteOne: "Do you really want to delete this task?",
+	deleteOneYes: "Yes, Delete it",
+	deleteOneNo: "No, Keep it",
+	deleteList: "Are you sure you want to delete that list?",
+	warning: "Warning!",
+
+	recurring: "Recurring",
+	reviewNo: "Review in:",
+	recurType: "Created:",
+	daily: "daily",
+	weekly: "weekly",
+	monthly: "monthly",
+	recurNext: "Next: ",
+	recurEnds: "Ends: ",
+	reviewAction: "and place in: ",
+	create: "Create",
+	cancel: "Cancel",
+
+	hideWarnings: "Hide delete warnings",
+	nextNoLists: "Don't show any tasks in next",
+	nextEverything:	"Show all tasks",
+
+	general: "General",
+	nextDescription: "Show other lists in Next: ", 
+	deleteWarningsDescription: "This disables the confirm box that appears when you delete a task or list.",
+
+	language: "Language",
+	authortext: "Author",
+
+	about: "About",
+	theme: "Megjelenés",
+	pickTheme: "Válasszon témát: ",
+	themeDescription: "A témák segítségével megváltoztathatja a Nitro megjelenését. Saját témákat is készíthet CSS segítségével!",
+	replaceDefault:"Háttérkép: ",
+	useDefault: "Alapértelmezett háttérkép használata: ",
+	removeBG: "Háttérkép eltávolítása",
+	bgSize: "Háttér mérete: ",
+	fill:"Kitöltés",
+	shrink:	"Átméretezés",
+	tile:"Csempézés",
+	bgDescription: "Beállíthatja, hogy hogyan jelenjen meg a háttérkép.",
+	headingColor: "Szöveg színe",
+	default: "Alapértelmezett",
+	light: "Világos",
+	dark:"Sötét",
+	headingDescription:	"Beállíthatja a szöveg színét, hogy bármilyen háttérkép használata esetén olvashatóan jelenjen meg.",
+	sync: "Szinkronizáció",
+	donateText:	"Sync is beta. If Nitro breaks, explodes or melts your computer - we warned you. It is pretty stable but if anything does break, you can contact <a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> or <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "Run sync: ",
+	syncDescription:"Sync allows you to access your tasks from other computers and smartphones.",
+	syncNever: "Never (No notifications)",
+	syncManual:	"Manually",
+	syncTimer: "Every 2 minutes",
+	syncAuto: "Every time a change is made",
+	syncLoggedIn: "Logged in as: ",
+	syncService:"Service: ",
+	syncLogout: "Logout",
+	syncAuthenticate: "Waiting for Nitro to be authenticated...",
+	syncError: "Could not sync with server...",
+	cancel: "Cancel"
+})
