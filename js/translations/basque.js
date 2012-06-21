29dbc4142a61039f300a1b3f7d2410cef13a256f
Jono Cooper <jono@jono-netbook.(none)>
2012-06-22 00:40:53 +1200

    Added Basque

---
+++ js/translations/basque.js
@@ -0,0 +1,104 @@
+ui.language({
+	//List Headers
+	focus: "Zerrenda nagusiak",
+	lists: "Zerrenda pertsonalak",
+	today: "Gaur",
+	next: "Hurrengoak",
+	scheduled: "Scheduled",
+	logbook: "Logbook",
+	all: "All Tasks",
+
+	//Default Names
+	nlist: "Zerrenda berria",
+	ntask: "Zeregin berria",
+
+	//Buttons
+	addbtn: "Gehitu",
+	deletebtn: "Ezabatu",
+	sortbtn: "Sort",
+
+	// Sort
+	sortMagic: "Magic",
+	sortDefault: "by Hand",
+	sortPriority: "Priority",
+	sortDate: "Date",
+
+	//Placeholder Text
+	search: "Bilatu",
+	notes: "Notak",
+
+	//Priority
+	none: "Lehentasunik ez",
+	low: "Lehentasun gutxi",
+	medium: "Lehentasun ertaina",
+	high: "Lehentasun handia",
+
+	//Date Labels
+	dueDate: "Due Date",
+	schedule: "Schedule",
+
+	// Delete Modals
+	deleteMore: "Do you really want to delete these %s tasks?",
+	deleteMoreYes: "Yes, Delete them",
+	deleteMoreNo: "No, Keep them",
+	deleteOne: "Do you really want to delete this task?",
+	deleteOneYes: "Yes, Delete it",
+	deleteOneNo: "No, Keep it",
+	deleteList: "Are you sure you want to delete that list?",
+	warning: "Warning!",
+
+	recurring: "Recurring",
+	reviewNo: "Review in:",
+	recurType: "Created:",
+	daily: "daily",
+	weekly: "weekly",
+	monthly: "monthly",
+	recurNext: "Next: ",
+	recurEnds: "Ends: ",
+	reviewAction: "and place in: ",
+	create: "Create",
+	cancel: "Cancel",
+
+	hideWarnings: "Hide delete warnings",
+	nextNoLists: "Don't show any tasks in next",
+	nextEverything:	"Show all tasks",
+
+	general: "General",
+	nextDescription: "Show other lists in Next: ", 
+	deleteWarningsDescription: "This disables the confirm box that appears when you delete a task or list.",
+
+	language: "Language",
+	authortext: "Author",
+
+	about: "About",
+	theme: "Theme",
+	pickTheme: "Pick a theme: ",
+	themeDescription: "Themes allow you to change the look of Nitro. You can even use CSS to create your own!",
+	replaceDefault:"Change the background: ",
+	useDefault: "Use the default background: ",
+	removeBG: "Remove background",
+	bgSize: "Background size: ",
+	fill:"Fill screen",
+	shrink:	"Shrink",
+	tile:"Tile",
+	bgDescription: "Change the color of the text so that it looks nice with your custom background.",
+	headingColor: "Heading color",
+	default: "Default",
+	light: "Light",
+	dark:"Dark",
+	headingDescription:	"Change the color of the text so that it looks nice with your custom background.",
+	sync: "Sync",
+	donateText:	"Sync is beta. If Nitro breaks, explodes or melts your computer - we warned you. It is pretty stable but if anything does break, you can contact <a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> or <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "Run sync: ",
+	syncDescription:"Sync allows you to access your tasks from other computers and smartphones.",
+	syncNever: "Never (No notifications)",
+	syncManual:	"Manually",
+	syncTimer: "Every 2 minutes",
+	syncAuto: "Every time a change is made",
+	syncLoggedIn: "Logged in as: ",
+	syncService:"Service: ",
+	syncLogout: "Logout",
+	syncAuthenticate: "Waiting for Nitro to be authenticated...",
+	syncError: "Could not sync with server...",
+	cancel: "Cancel"
+})
