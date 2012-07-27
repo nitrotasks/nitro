31b8a5ee4e77f1f96ef190760e0f26a710e54dde
Erik Ammerlaan <erik.am@solcon.nl>
2012-07-27 14:33:37 +0200

    Added Dutch translation

---
+++ js/translations/dutch.js
@@ -0,0 +1,176 @@
+// Nederlands (Dutch)
+
+ui.language({
+	//List Headers
+	focus: "In beeld",
+	lists: "Lijstjes",
+	today: "Vandaag",
+	next: "Later",
+	scheduled: "Gepland",
+	logbook: "Logboek",
+	all: "Alle taken",
+
+	// Empty List Filler Text
+	noTasksInList: "Geen taken in %s",
+	thisList: "Dit lijstje",
+
+	//Default Names
+	nlist: "Nieuw lijstje",
+	ntask: "Nieuwe taak",
+
+	//Buttons
+	addbtn: "Toevoegen",
+	deletebtn: "Verwijderen",
+	sortbtn: "Sorteren",
+	addList: "Lijstje toevoegen",
+	moveToLogbookSingle: "1 voltooide taak naar het logboek verplaatsen",
+	moveToLogbookPlural: "%s voltooide taken naar het logboek verplaatsen",
+
+	// Sort
+	sortMagic: "Magisch",
+	sortDefault: "Handmatig",
+	sortPriority: "Op prioriteit",
+	sortDate: "Op datum",
+
+	//Placeholder Text
+	search: "Zoeken",
+	notes: "Notities",
+
+	//Priority
+	none: "Geen",
+	low: "Laag",
+	medium: "Midden",
+	high: "Hoog",
+
+	//Date Labels
+	dueDate: "Uiterste datum",
+	schedule: "Plannen",
+
+	// Due Date Labels
+	daysOverdue: "%s dagen over tijd",
+	dueYesterday: "uiterlijk gisteren",
+	dueToday: "uiterlijk vandaag",
+	dueTomorrow: "uiterlijk morgen",
+	daysLeft: "nog %s dagen",
+
+	// Days and Months
+	sunday: "zondag",
+	monday: "maandag",
+	tuesday: "dinsdag",
+	wednesday: "woensdag",
+	thursday: "donderdag",
+	friday: "vrijdag",
+	saturday: "zaterdag",
+
+	sunShort: "zo",
+	monShort: "ma",
+	tueShort: "di",
+	wedShort: "wo",
+	thuShort: "do",
+	friShort: "vr",
+	satShort: "za",
+
+	sunMin: "zo",
+	monMin: "ma",
+	tueMin: "di",
+	wedMin: "wo",
+	thuMin: "do",
+	friMin: "vr",
+	satMin: "za",
+
+	january: "januari",
+	february: "februari",
+	march: "maart",
+	april: "april",
+	may: "mei",
+	june: "juni",
+	july: "juli",
+	august: "augustus",
+	september: "september",
+	october: "oktober",
+	november: "november",
+	december: "december",
+
+	janShort: "jan",
+	febShort: "feb",
+	marShort: "mrt",
+	aprShort: "apr",
+	mayShort: "mei",
+	junShort: "jun",
+	julShort: "jul",
+	augShort: "aug",
+	sepShort: "sep",
+	octShort: "okt",
+	novShort: "nov",
+	decShort: "dec",
+
+	// Delete Modals
+	deleteMore: "Wilt u deze %s taken echt verwijderen?",
+	deleteMoreYes: "Ja, verwijder ze.",
+	deleteMoreNo: "Nee, bewaar ze.",
+	deleteOne: "Wilt u deze taak echt verwijderen?",
+	deleteOneYes: "Ja, verwijder hem.",
+	deleteOneNo: "Nee, bewaar hem.",
+	deleteList: "Wilt u dat lijstje echt verwijderen?",
+	warning: "Opgelet!",
+
+	recurring: "Periodiek",
+	reviewNo: "Controleren in:",
+	recurType: "Opnieuw:",
+	daily: "dagelijks",
+	weekly: "wekelijks",
+	monthly: "maandelijks",
+	recurNext: "Volgende keer: ",
+	recurEnds: "Stopt op: ",
+	reviewAction: "en plaatsen in: ",
+	create: "Maken",
+	cancel: "Annuleren",
+
+	hideWarnings: "Waarschuwingen bij verwijderen verbergen",
+	nextNoLists: "Geen taken bij Later tonen",
+	nextEverything:	"Alle taken tonen",
+
+	general: "Algemeen",
+	nextDescription: "Andere lijstjes bij Later tonen:", 
+	deleteWarningsDescription: "Dit voorkomt een melding bij het verwijderen van een taak of lijstje.",
+
+	//Clear Data
+	resetnitro: "Nitro resetten: ",
+	cleardata: "Gegevens verwijderen",
+	clearDataMsg: "Weet u zeker dat u Nitro wilt resetten? Uw gegevens zullen voor altijd verloren zijn (dat is lang).",
+
+	language: "Taal",
+	authortext: "Auteur",
+
+	about: "Over",
+	theme: "Thema",
+	pickTheme: "Kies een thema uit: ",
+	themeDescription: "Met thema's kunt u de uitstraling van Nitro veranderen. Je kunt zelfs CSS gebruiken om je eigen thema te maken!",
+	replaceDefault:"Achtergrond veranderen: ",
+	useDefault: "Gebruik de standaardachtergrond: ",
+	removeBG: "Achtergrond verwijderen",
+	bgSize: "Achtergrondgrootte: ",
+	fill:"Schermvullend",
+	shrink:	"Passend",
+	tile:"Tegels",
+	bgDescription: "Verander de tekstkleur, zodat die goed bij de achtergrond past.",
+	headingColor: "Kopkleur",
+	default: "Standaard",
+	light: "Licht",
+	dark:"Donker",
+	headingDescription:	"Verander de tekstkleur, zodat die goed bij de achtergrond past.",
+	sync: "Synchroniseren",
+	donateText:	"Synchroniseren is in beta. Als door Nitro je computer kapotgaat, ontploft of smelt - we hebben je gewaarschuwd. Het is vrij stabiel, maar mocht er toch iets niet werken, kun je contact opnemen met <a href='https://twitter.com/GeorgeCzabania'>@GeorgeCzabania</a> of <a href='https://twitter.com/consindo'>@consindo",
+	syncLabel: "Synchroniseren: ",
+	syncDescription:"Door Synchroniseren kun je bij je taken vanaf andere computers of smartphones.",
+	syncNever: "Nooit",
+	syncManual:	"Handmatig",
+	syncTimer: "Elke 30 seconden",
+	syncAuto: "Na elke verandering",
+	syncLoggedIn: "Ingelogd als: ",
+	syncService:"Dienst: ",
+	syncLogout: "Uitloggen",
+	syncAuthenticate: "Aan het wachten tot Nitro geauthenticeerd is...",
+	syncError: "Kon niet synchroniseren met de server..",
+	cancel: "Annuleren"
+})
