db90b0ffabf5534c2c0052190d84fdec057b8423
Jono Cooper <jono@jonocooper.com>
2012-08-18 16:09:30 +1200

    Cross Browser CSS Spinner

--- js/plugins/settings.js
+++ js/plugins/settings.js
@@ -142,7 +142,7 @@ $(function() {
 					</div>\
 					<div class="waiting">\
 						<p><span class="translate" data-translate="syncAuthenticate"> </span><a class="cancel translate" data-translate="cancel"></a></p>\
-						<img class="spinner" src="css/img/spinner.gif">\
+						<div class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div>\
 					</div>\
 					<div class="settings">\
 						<a class="left logout translate" data-translate="syncLogout" href="#"></a>\
