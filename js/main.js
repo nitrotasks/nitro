42bbcddacd7ed8b6ba984b311f530744f7bfb850
Jono <jono@jonocooper.com>
2012-09-03 22:16:24 +1200

    Download Button for Web Version

--- js/main.js
+++ js/main.js
@@ -96,6 +96,9 @@ var ui = {
 			var loader = '<div id="syncloader" class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div>'
 			$('body').css('background', '#fff').append(loader)
 
+			//Download Button
+			$('#tasks .panel .right').prepend('<a href="http://nitrotasks.com" title="Download Nitro" class="downloadbtn"></a>')
+
 			//No settings but language
 			$('[data-target=#tabLanguage]').click()
 
