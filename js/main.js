5d1c5defdb74e0e754b8ca780d1ec5031f99f833
Jono <jono@jonocooper.com>
2012-09-05 19:04:26 +1200

    Check Browser Support

--- js/main.js
+++ js/main.js
@@ -94,7 +94,7 @@ var ui = {
 			// Run sync if out shit is loaded
 			$('html').addClass('web')
 			var loader = '<div id="syncloader" class="spinner"><div class="bar1"></div><div class="bar2"></div><div class="bar3"></div><div class="bar4"></div><div class="bar5"></div><div class="bar6"></div><div class="bar7"></div><div class="bar8"></div><div class="bar9"></div><div class="bar10"></div><div class="bar11"></div><div class="bar12"></div></div>'
-			$('body').css('background', '#fff').append(loader)
+			$('body').css('background', '#fff').append(loader).append('<script src="js/libs/modernizr.min.js"></script>')
 
 			//Download Button
 			$('#tasks .panel .right').prepend('<a href="http://nitrotasks.com" title="Download Nitro" class="downloadbtn"></a>')
