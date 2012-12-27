4367a831fd93fbdab01cbee4f4f0493649efc93d
Jono Cooper <jono@jonocooper.com>
2012-12-28 11:34:23 +1300

    Maybe we'll do translations when the app is all complete =)

--- public/i18n/datepicker/fr.js
+++ public/i18n/datepicker/fr.js
@@ -1,22 +1,25 @@
 /* French initialisation for the jQuery UI date picker plugin. */
-/* Written by Keith Wood (kbwood@virginbroadband.com.au) and Stéphane Nahmani (sholby@sholby.net). */
+/* Written by Keith Wood (kbwood{at}iinet.com.au),
+			  Stéphane Nahmani (sholby@sholby.net),
+			  Stéphane Raimbault <stephane.raimbault@gmail.com> */
 jQuery(function($){
-	$.datepicker.regional['fr'] = {clearText: 'Effacer', clearStatus: '',
-		closeText: 'Fermer', closeStatus: 'Fermer sans modifier',
-		prevText: '<Préc', prevStatus: 'Voir le mois précédent',
-		nextText: 'Suiv>', nextStatus: 'Voir le mois suivant',
-		currentText: 'Courant', currentStatus: 'Voir le mois courant',
+	$.datepicker.regional['fr'] = {
+		closeText: 'Fermer',
+		prevText: 'Précédent',
+		nextText: 'Suivant',
+		currentText: 'Aujourd\'hui',
 		monthNames: ['Janvier','Février','Mars','Avril','Mai','Juin',
 		'Juillet','Août','Septembre','Octobre','Novembre','Décembre'],
-		monthNamesShort: ['Jan','Fév','Mar','Avr','Mai','Jun',
-		'Jul','Aoû','Sep','Oct','Nov','Déc'],
-		monthStatus: 'Voir un autre mois', yearStatus: 'Voir un autre année',
-		weekHeader: 'Sm', weekStatus: '',
+		monthNamesShort: ['Janv.','Févr.','Mars','Avril','Mai','Juin',
+		'Juil.','Août','Sept.','Oct.','Nov.','Déc.'],
 		dayNames: ['Dimanche','Lundi','Mardi','Mercredi','Jeudi','Vendredi','Samedi'],
-		dayNamesShort: ['Dim','Lun','Mar','Mer','Jeu','Ven','Sam'],
-		dayNamesMin: ['Di','Lu','Ma','Me','Je','Ve','Sa'],
-		dayStatus: 'Utiliser DD comme premier jour de la semaine', dateStatus: 'Choisir le DD, MM d',
-		dateFormat: 'dd/mm/yy', firstDay: 0,
-		initStatus: 'Choisir la date', isRTL: false};
+		dayNamesShort: ['Dim.','Lun.','Mar.','Mer.','Jeu.','Ven.','Sam.'],
+		dayNamesMin: ['D','L','M','M','J','V','S'],
+		weekHeader: 'Sem.',
+		dateFormat: 'dd/mm/yy',
+		firstDay: 1,
+		isRTL: false,
+		showMonthAfterYear: false,
+		yearSuffix: ''};
 	$.datepicker.setDefaults($.datepicker.regional['fr']);
 });
