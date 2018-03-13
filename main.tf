a47337c12240acc5dc1a136e7232149276f9085e
Jono Cooper <jono@jonocooper.com>
2018-03-14 00:45:58 +1300

    Adjust Terraform to Pull From Docker Hub

--- main.tf
+++ main.tf
@@ -3,11 +3,18 @@ provider "docker" {
 }
 
 resource "docker_container" "nitro-client" {
-  image = "nitro-client"
+  image = "${docker_image.nitro-client.latest}"
   name = "nitro-client"
   networks = ["nitro_private_network"]
   ports {
     internal = 80
     external = 80
   }
+}
+data "docker_registry_image" "nitro-client" {
+  name = "dymajo/nitro-client:latest"
+}
+resource "docker_image" "nitro-client" {
+  name          = "${data.docker_registry_image.nitro-client.name}"
+  pull_triggers = ["${data.docker_registry_image.nitro-client.sha256_digest}"]
 }
