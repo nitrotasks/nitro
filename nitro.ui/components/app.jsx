f1204b9306f6628bb074d9423f2e0d2c87a51659
Jono Cooper <jono@jonocooper.com>
2018-04-07 00:41:45 +0000

    Add React Transition Group

--- nitro.ui/components/app.jsx
+++ nitro.ui/components/app.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { View } from 'react-native'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { TransitionGroup, Transition } from 'react-transition-group'
 
 import { Lists } from './lists/index.jsx'
 import { Tasks } from './tasks/index.jsx'
@@ -10,15 +11,45 @@ class App extends React.Component {
   render() {
     return (
       <View>
-        <BrowserRouter>
-          <Switch>
-            <Route path="/:list/:task" component={Editor} />
-            <Route path="/:list" component={Tasks} />
-            <Route path="/" component={Lists} />
-          </Switch>
-        </BrowserRouter>
+        <MainNavigation />
       </View>
     )
   }
 }
+const MainNavigation = ({ children }) => (
+  <BrowserRouter>
+    <Route
+      render={({ location }) => (
+        <TransitionGroup>
+          <Transition key={location.key} timeout={300}>
+            {state => {
+              return (
+                <Switch location={location}>
+                  <Route
+                    path="/:list/:task"
+                    render={routeProps => (
+                      <Editor {...routeProps} transitionState={state} />
+                    )}
+                  />
+                  <Route
+                    path="/:list"
+                    render={routeProps => (
+                      <Tasks {...routeProps} transitionState={state} />
+                    )}
+                  />
+                  <Route
+                    path="/"
+                    render={routeProps => (
+                      <Lists {...routeProps} transitionState={state} />
+                    )}
+                  />
+                </Switch>
+              )
+            }}
+          </Transition>
+        </TransitionGroup>
+      )}
+    />
+  </BrowserRouter>
+)
 export default App
