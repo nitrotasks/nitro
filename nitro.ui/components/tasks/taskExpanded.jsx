70a49c47240133d6b5c9a6159f0ea2f1200f8be7
Jono Cooper <jono@jonocooper.com>
2018-04-16 09:29:19 +0000

    Reasonable Animation for Overlay

--- nitro.ui/components/tasks/taskExpanded.jsx
+++ nitro.ui/components/tasks/taskExpanded.jsx
@@ -17,10 +17,15 @@ export class TaskExpanded extends React.Component {
     this.wrapper = React.createRef()
 
     const task = this.getTask(props)
-    this.state = { ...task }
+    this.state = { ...task, hidden: true }
   }
   componentDidMount() {
     this.measureWrapper()
+    requestAnimationFrame(() => {
+      this.setState({
+        hidden: false
+      })
+    })
   }
   getTask = (props = this.props) => {
     const task = NitroSdk.getTask(props.match.params.task)
@@ -63,11 +68,25 @@ export class TaskExpanded extends React.Component {
   render() {
     let top = 0
     if (this.props.position !== null) {
-      top = this.props.position
+      top = this.props.position + vars.padding * 2
+    }
+    let opacity = 1
+    let overlayOpacity = 0.5
+    let transform = [{ translateY: 0 }]
+    if (this.state.hidden) {
+      opacity = 0
+      overlayOpacity = 0
+      transform = [{ translateY: -2 * vars.padding }]
     }
     return (
       <React.Fragment>
-        <View style={[styles.wrapper, { top: top }]} ref={this.wrapper}>
+        <View
+          style={[
+            styles.wrapper,
+            { top: top, opacity: opacity, transform: transform }
+          ]}
+          ref={this.wrapper}
+        >
           <View style={styles.topRow}>
             <Checkbox
               checked={this.state.checked}
@@ -92,7 +111,7 @@ export class TaskExpanded extends React.Component {
           />
         </View>
         <View
-          style={[styles.overlay, { top: top - 100 }]}
+          style={[styles.overlay, { top: top - 100, opacity: overlayOpacity }]}
           onClick={this.triggerOverlay}
         />
       </React.Fragment>
@@ -107,7 +126,10 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100vh',
     backgroundColor: '#fff',
-    opacity: 0.5
+    opacity: 0.5,
+    transitionDuration: '200ms',
+    transitionTimingFunction: 'ease',
+    transitionProperty: 'opacity'
   },
   wrapper: {
     position: 'absolute',
@@ -116,7 +138,10 @@ const styles = StyleSheet.create({
     width: '100%',
     backgroundColor: '#fff',
     padding: vars.padding,
-    boxShadow: '0 1px 3px rgba(0,0,0,0.3)'
+    boxShadow: '0 1px 3px rgba(0,0,0,0.3)',
+    transitionDuration: '300ms, 300ms',
+    transitionTimingFunction: 'ease, ease',
+    transitionProperty: 'opacity, transform'
   },
   topRow: {
     flex: 1,
