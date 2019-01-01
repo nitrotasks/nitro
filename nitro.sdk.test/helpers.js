d975af024e74ee9c9a4164489d4cef93970dfedd
Jono Cooper <jono@jonocooper.com>
2019-01-01 18:01:18 +1300

    Add Unit Tests for Check Status Helper

--- nitro.sdk.test/helpers.js
+++ nitro.sdk.test/helpers.js
@@ -1,14 +1,15 @@
 import assert from 'assert'
 
 import { promiseSerial } from '../nitro.sdk/helpers/promise.js'
+import { checkStatus } from '../nitro.sdk/helpers/fetch.js'
 
-describe('helpers', function() {
-  describe('promiseSerial', function() {
-    it('should run async functions in serial', function(done) {
+describe('helpers', () => {
+  describe('promiseSerial', () => {
+    it('should run async functions in serial', done => {
       let whatever = '0'
       const funcs = [
         () => {
-          return new Promise((resolve) => {
+          return new Promise(resolve => {
             setTimeout(() => {
               whatever = '1'
               resolve()
@@ -24,6 +25,30 @@ describe('helpers', function() {
         assert.equal(whatever, '2')
         done()
       })
+    })
+  })
+
+  describe('checkStatus', () => {
+    it('should pass through if the response is successful', done => {
+      const response = { status: 200 }
+      Promise.resolve(response)
+        .then(checkStatus)
+        .then(res => {
+          assert.equal(res, response)
+          done()
+        })
+    })
+    it('should return an error with json if the status code indicates an error', done => {
+      const msg = { message: 'some message' }
+      const response = { status: 500, json: () => Promise.resolve(msg) }
+      Promise.resolve(response)
+        .then(checkStatus)
+        .catch(err => {
+          assert.equal(err instanceof Error, true)
+          assert.equal(err.status, response.status)
+          assert.equal(JSON.stringify(err.response), JSON.stringify(msg))
+          done()
+        })
     })
   })
 })
