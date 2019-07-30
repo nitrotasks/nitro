03fd8db12617b55eab8b1eec5ac120fc8a35d5d8
Jono Cooper <jono@jonocooper.com>
2019-07-30 15:11:32 +1200

    Deploy Lambda in CI

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+echo "Building Go Proxy..."
+cd ./nitro.web
+go build
+
+echo "Zipping Go Proxy..."
+zip ../deployment/bin/deployment.zip ./nitro.web
+cd ..
+
+echo "Zipping JS Bundle..."
+zip -r deployment/bin/deployment.zip ./dist
+
+echo "Installing Terraform"
+curl https://releases.hashicorp.com/terraform/0.12.5/terraform_0.12.5_linux_amd64.zip -o terraform.zip
+unzip -o terraform.zip 
+
+echo "Terraform Init..."
+cd deployment/env/$1
+../../../terraform init
+
+echo "Terraform Apply..."
+../../../terraform apply --auto-approve
